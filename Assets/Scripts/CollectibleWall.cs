a8193f1b573a872846bbefca6439f068ae2423a7
cebner <giix09@gmail.com>
2019-02-06 02:25:07 +0100

    wall

--- Assets/Scripts/CollectibleWall.cs
+++ Assets/Scripts/CollectibleWall.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class CollectibleWall : MonoBehaviour {
-        
+public class CollectibleWall : MonoBehaviour
+{
     void OnTriggerEnter(Collider other) {
         var playerLogic = other.transform.parent.gameObject.GetComponent<PlayerLogic>();
 
         //collided with player
         if (playerLogic != null /* and player local */) {
             playerLogic.ActionWall();
-            Destroy(gameObject);
+            var ps = transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
+            ps.Play();
+            GetComponent<Collider>().enabled = false;
+            GetComponent<MeshRenderer>().enabled = false;
+            Destroy(gameObject,3);
         }
     }
 }
